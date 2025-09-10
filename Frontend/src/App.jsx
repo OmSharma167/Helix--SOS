4c2781bb6e8bb6c97cb8a796d79ace5fdb86670f
OmSharma167 <op809034@gmail.com>
2025-09-10 20:28:21 +0530

    feat: update App.jsx and add Hero component
    - Modified App.jsx to integrate new changes
    - Added initial Hero component folder structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,42 +1,38 @@
-
-
-
 import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import DoctorFormPage from "./pages/DoctorFormPage";
 import DoctorListPage from "./pages/DoctorListPage";
 import BookDoctorPage from "./pages/BookDoctorPage";
-import HospitalRegistrationForm from "./pages/HospitalRegistrationForm";
-import PoliceStationRegistrationForm from "./pages/PoliceStationRegistrationForm";
+import MyBookingsPage from "./pages/MyBookingsPage";
+import DoctorBookingsPage from "./pages/DoctorBookingsPage";
+// import HospitalRegistrationForm from "./pages/HospitalRegistrationForm";
+// import PoliceStationRegistrationForm from "./pages/PoliceStationRegistrationForm";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/doctor/register" element={<DoctorFormPage />} />
-        <Route path="/doctors" element={<DoctorListPage />} />
-        <Route path="/book-doctor/:id" element={<BookDoctorPage />} />
-        <Route path="/my-bookings" element={<MyBookingsPage />} />
-        <Route path="/doctor/bookings" element={<DoctorBookingsPage />} />
-        <Route
-          path="/hospital-registration"
-          element={<HospitalRegistrationForm />}
-        />
-        <Route
-          path="/police-registration"
-          element={<PoliceStationRegistrationForm />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/doctor/register" element={<DoctorFormPage />} />
+      <Route path="/doctors" element={<DoctorListPage />} />
+      <Route path="/book-doctor/:id" element={<BookDoctorPage />} />
+      <Route path="/my-bookings" element={<MyBookingsPage />} />
+      <Route path="/doctor/bookings" element={<DoctorBookingsPage />} />
+      {/* <Route
+        path="/hospital-registration"
+        element={<HospitalRegistrationForm />}
+      />
+      <Route
+        path="/police-registration"
+        element={<PoliceStationRegistrationForm />}
+      /> */}
+    </Routes>
   );
 }
 
 export default App;
-
